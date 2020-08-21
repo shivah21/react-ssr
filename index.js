95ffa307328b3103afec9b78394ff1b89f025deb
Shiva Kumar <shivah21@gmail.com>
2020-08-21 11:54:04 +0100

    babel-register package removed

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-require( "babel-register" )( {
-    presets: [ "env" ],
-    plugins: [
-        [
-            "css-modules-transform",
-            {
-                camelCase: true,
-                extensions: [ ".css", ".scss" ],
-            },
-        ],
-        "dynamic-import-node",
-    ],
-} );
-
 module.exports = require( "./src/server.js" );
