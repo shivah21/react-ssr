5cb5ba9c3442e4a9902d81e74a2090686d614fa6
Shiva Kumar <shivah21@gmail.com>
2020-08-22 10:55:54 +0100

    Modified app index

--- app/index.js
+++ app/index.js
@@ -1,14 +1,16 @@
-require( "babel-register" )( {
-    presets: [ "../env" ],
-    plugins: [
-        [
-            "css-modules-transform",
-            {
-                camelCase: true,
-                extensions: [ ".css", ".scss" ],
-            },
-        ],
-        "dynamic-import-node",
-    ],
-});
-require( "../src/server" );
+require('../index.js');
+
+// require( "babel-register" )( {
+//     presets: [ "env" ],
+//     plugins: [
+//         [
+//             "css-modules-transform",
+//             {
+//                 camelCase: true,
+//                 extensions: [ ".css", ".scss" ],
+//             },
+//         ],
+//         "dynamic-import-node",
+//     ],
+// });
+// require( "../src/server" );
